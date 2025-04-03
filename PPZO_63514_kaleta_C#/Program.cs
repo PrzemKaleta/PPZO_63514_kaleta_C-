d48a97b2d20025abffa9819d2c888331c16ea8ac
PrzemKaleta <przemyslaw.kaleta@student.wsb.edu.pl>
2025-04-03 21:36:17 +0200

    Dodaj pliki projektów.

---
+++ PPZO_63514_kaleta_C#/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Menu");
+        Console.WriteLine("1 - Prosty kalkulator dwoch liczb");
+        Console.WriteLine("2 - Konwerter temperatur (Celsjusz ↔ Fahrenheit)");
+        Console.WriteLine("3 - Srednia ocen ucznia");
+
+        Console.Write("Opcja: ");
+        int menu_option = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Menu(menu_option));
+    }
+
+    static string Menu(int menu_option)
+    {
+        switch (menu_option)
+        {
+            case 1:
+                Console.WriteLine("Prosty kalkulator dwoch liczb");
+
+                Console.Write("Podaj pierwsza liczbe: ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("Podaj druga liczbe: ");
+                int b = int.Parse(Console.ReadLine());
+
+                Console.Write("Podaj jaka operacje arytmetyczna chcesz wykonac (+, -, *, /): ");
+                string zad1_option = Console.ReadLine();
+
+                double wynik1 = 0;
+
+                if (zad1_option == "+")
+                {
+                    wynik1 = a + b;
+                }
+                else if (zad1_option == "-")
+                {
+                    wynik1 = a - b;
+                }
+                else if (zad1_option == "*")
+                {
+                    wynik1 = a * b;
+                }
+                else if (zad1_option == "/")
+                {
+                    if (b != 0)
+                    {
+                        wynik1 = a / (double)b;
+                    }
+                    else
+                    {
+                        return "Nie mozna dzielic przez 0";
+                    }
+                }
+                else
+                {
+                    return "Bledna operacja arytmetyczna";
+                }
+
+                return "Wynik = " + wynik1;
+            case 2:
+                //Console.WriteLine("Konwerter temperatur (Celsjusz ↔ Fahrenheit)");
+            case 3:
+                //Console.WriteLine("Srednia ocen ucznia");
+            default:
+                return "Niepoprawna opcja";
+        }
+    }
+}
